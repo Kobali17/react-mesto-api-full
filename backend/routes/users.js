46d904c1272fe963d22803c1b6e645d7f00a5161
Luna <Kobali17@yandex.ru>
2020-12-17 23:29:53 +0300

    backend: register /me before /:id

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -7,19 +7,19 @@ const {
 
 router.get('/users', getUsers);
 
-router.get('/users/:id', celebrate({
-  params: Joi.object().keys({
-    id: Joi.string().required().alphanum().length(24),
-  }),
-
-}), getUser);
-
 router.patch('/users/me', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
     about: Joi.string().min(2).max(30),
   }),
 }), updateUserInfo);
+
+router.get('/users/:id', celebrate({
+  params: Joi.object().keys({
+    id: Joi.string().required().alphanum().length(24),
+  }),
+
+}), getUser);
 
 router.get('/users/me', getUserInfo);
 router.patch('/users/me/avatar', celebrate({
