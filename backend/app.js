21c18b84c1970f6abf44de1bd8b343c60711707b
Luna <Kobali17@yandex.ru>
2020-12-17 22:40:57 +0300

    backend: log 500 error messages

--- backend/app.js
+++ backend/app.js
@@ -41,6 +41,9 @@ app.use(errorLogger);
 app.use(errors());
 app.use((err, req, res, next) => {
   const { statusCode = 500, message } = err;
+  if (statusCode === 500) {
+    console.log(message);
+  }
   res.status(statusCode)
     .send({
       message: statusCode === 500
