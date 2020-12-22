df4821df80a3f4a55178b287099a59394c301997
Luna <Kobali17@yandex.ru>
2020-12-22 11:22:08 +0300

    bugfix

--- backend/controllers/cards.js
+++ backend/controllers/cards.js
@@ -19,7 +19,7 @@ module.exports.getCards = (req, res, next) => {
 module.exports.deleteCard = (req, res, next) => {
   Card.findById(req.params.cardId).orFail(new NotFoundError('Карточка не найдена')).then((card) => {
     if (card.owner.toString() === req.user._id.toString()) {
-      Card.findByIdAndRemove(card.id);
+      Card.findByIdAndDelete(card.id);
       res.status(200).send(card);
     } else {
       throw new ForbiddenError('Недостаточно прав');
